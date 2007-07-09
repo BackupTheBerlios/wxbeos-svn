91b583db993fa3af74b92cc5333f7c86c4141a73
wxknorr <wxknorr@06622bfc-d729-0410-b73a-e94e1f74d5d8>
2007-07-09 14:21:27 +0000

    commit src/ without subdirs

---
+++ trunk/src/extended.d
@@ -0,0 +1,12 @@
+extended.o: \
+ /boot/home/files/projects/wxWidgets-2.6.3/src/common/extended.c \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/defs.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/platform.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/setup.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/chkconf.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/features.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/version.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/dlimpexp.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/debug.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/wxchar.h \
+ /boot/home/files/projects/wxWidgets-2.6.3/include/wx/math.h
